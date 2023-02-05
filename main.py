3b84d92b37d8fb268b39076dbd919c9d09bf444d
Mohsen <mohsen.minavand1384@gmail.com>
2023-02-05 22:18:20 +0330

    create config file creator func

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+
+
+from os import path
+
+
+
+def CreateConfig() : 
+    # Create cloudflare config file . 
+    if path.exists('CONFIG.py') : 
+        return 
+       
+    Zone = input('Enter your domain(zone) in CloudFlare : ') 
+    DnsRecord = input('Enter your subdomain in CloudFlare : ')
+    Email = input('Enter your CloudFlare email : ')
+    AuthKey = input('Enter your CloudFlare Auth-Key : ')
+
+    with open('CONFIG.py' , 'w') as File :    
+        File.write('#Made With <3')
+        File.write(f'\nZone = {Zone}')
+        File.write(f'\nDnsRecord = {DnsRecord}')
+        File.write(f'\nEmail = {Email}')
+        File.write(f'\nAuthKey = {AuthKey}')
+
+
+
+if __name__ == '__main__' :
+    CreateConfig()
