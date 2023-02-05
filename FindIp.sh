19338b2f0b7baec7b19622881644aa0897f160cc
Mohsen <mohsen.minavand1384@gmail.com>
2023-02-05 23:01:30 +0330

    add IpScanner from mortezabashsiz https://github.com/MortezaBashsiz/CFScanner and little edit on it .

---
+++ FindIp.sh
@@ -0,0 +1,104 @@
+#!/bin/bash  -
+#===============================================================================
+#
+#          FILE: cfFindIP.sh
+#
+#         USAGE: ./cfFindIP.sh [ThreadCount]
+#
+#   DESCRIPTION: Scan all 1.5 Mil CloudFlare IP addresses
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ThreadCount (integer Number which defines the parallel processes count)
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Morteza Bashsiz (mb), morteza.bashsiz@gmail.com
+#  ORGANIZATION: Linux
+#       CREATED: 01/24/2023 07:36:57 PM
+#      REVISION:  1 by Nomad
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
+# Check if 'parallel', 'nmap' and 'bc' packages are installed
+# If they are not,exit the script
+
+if [[ "$(uname)" == "Linux" ]]; then
+    command -v parallel >/dev/null 2>&1 || { echo >&2 "I require 'parallel' but it's not installed. Please install it and try again."; exit 1; }
+    command -v nmap >/dev/null 2>&1 || { echo >&2 "I require 'nmap' but it's not installed. Please install it and try again."; exit 1; }
+    command -v bc >/dev/null 2>&1 || { echo >&2 "I require 'bc' but it's not installed. Please install it and try again."; exit 1; }
+		command -v timeout >/dev/null 2>&1 || { echo >&2 "I require 'timeout' but it's not installed. Please install it and try again."; exit 1; }
+
+elif [[ "$(uname)" == "Darwin" ]];then
+    command -v parallel >/dev/null 2>&1 || { echo >&2 "I require 'parallel' but it's not installed. Please install it and try again."; exit 1; }
+    command -v nmap >/dev/null 2>&1 || { echo >&2 "I require 'nmap' but it's not installed. Please install it and try again."; exit 1; }
+    command -v bc >/dev/null 2>&1 || { echo >&2 "I require 'bc' but it's not installed. Please install it and try again."; exit 1; }
+    command -v gtimeout >/dev/null 2>&1 || { echo >&2 "I require 'gtimeout' but it's not installed. Please install it and try again."; exit 1; }
+fi
+
+
+threads="$1"
+echo "Start testing for $2"
+
+cloudFlareIpList=$(cat $3)
+
+now=$(date +"%Y%m%d-%H%M%S")
+scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+resultFile="$scriptDir/$now-result-$2.cf"
+
+
+# Function fncCheckSubnet
+# Check Subnet
+function fncCheckSubnet {
+	local ipList resultFile timeoutCommand domainFronting
+	ipList="$1"
+	resultFile="$2"
+	# set proper command for linux
+	if command -v timeout >/dev/null 2>&1; 
+	then
+	    timeoutCommand="timeout"
+	else
+		# set proper command for mac
+		if command -v gtimeout >/dev/null 2>&1; 
+		then
+		    timeoutCommand="gtimeout"
+		else
+		    echo >&2 "I require 'timeout' command but it's not installed. Please install 'timeout' or an alternative command like 'gtimeout' and try again."
+		    exit 1
+		fi
+	fi
+	for ip in ${ipList}
+		do
+			if $timeoutCommand 1 bash -c "</dev/tcp/$ip/443" > /dev/null 2>&1;
+			then
+				domainFronting=$($timeoutCommand 2 curl -s -w "%{http_code}\n" --tlsv1.2 -servername fronting.sudoer.net -H "Host: fronting.sudoer.net" --resolve fronting.sudoer.net:443:"$ip" https://fronting.sudoer.net -o /dev/null | grep '200')
+				if [[ "$domainFronting" == "200" ]]
+				then
+					timeMil=$($timeoutCommand 2 curl -s -w "TIME: %{time_total}\n" --tlsv1.2 -servername scan.sudoer.net -H 'Host: scan.sudoer.net' --resolve scan.sudoer.net:443:"$ip" https://scan.sudoer.net | grep "TIME" | tail -n 1 | awk '{print $2}' | xargs -I {} echo "{} * 1000 /1" | bc )
+					if [[ "$timeMil" ]] 
+					then
+						echo "OK $ip ResponseTime $timeMil" 
+						echo "$timeMil $ip" >> "$resultFile"
+					else
+						echo "FAILED $ip"
+					fi
+				else
+					echo "FAILED $ip"
+				fi
+			else
+				echo "FAILED $ip"
+			fi
+	done
+}
+# End of Function fncCheckSubnet
+export -f fncCheckSubnet
+
+echo "" > "$resultFile"
+
+for subNet in ${cloudFlareIpList}
+do
+	ipList=$(nmap -sL -n "$subNet" | awk '/Nmap scan report/{print $NF}')
+	parallel -j "$threads" fncCheckSubnet ::: "$ipList" ::: "$resultFile"
+done
+
+
+sort -n -k1 -t, "$resultFile" -o "$resultFile"
